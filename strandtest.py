bb41b770d5486b053ab0d5c99415cf9f6d339918
Kyle Thomson <styplanekyle@gmail.com>
2025-11-14 03:44:52 -0500

    s

--- strandtest.py
+++ strandtest.py
@@ -19,81 +19,19 @@ LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
+def renderPieShowFile(strip, bin)
+    #parse len, fps, ect.
+    print(list(bin))
+    
+    #loop through all leds in led len
+    
+    #show
+    
+    #end when done
 
 def setAll(strip, color):
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, Color(255,255,255))
-
-def strobe(strip, ontime):
-    setAll(strip, Color(255,255,255))
-    strip.show()
-    time.sleep(ontime / 1000.0)
-    setAll(strip, Color(0,0,0))
-    strip.show()
-    time.sleep(ontime / 1000.0)
-
-
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, color)
-            strip.show()
-            time.sleep(wait_ms / 1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, 0)
-
-
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
-
-
-def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
-    for j in range(256 * iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((i + j) & 255))
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-
-
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-    """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256 * iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel(
-                (int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-
-
-def theaterChaseRainbow(strip, wait_ms=50):
-    """Rainbow movie theater light style chaser animation."""
-    for j in range(256):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, wheel((i + j) % 255))
-            strip.show()
-            time.sleep(wait_ms / 1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i + q, 0)
 
 
 # Main program logic follows:
@@ -101,6 +39,7 @@ if __name__ == '__main__':
     # Process arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    parser.add_argument('-l', '--loop', action='store_true', help='loop the file on completion')
     args = parser.parse_args()
 
     # Create NeoPixel object with appropriate configuration.
@@ -113,24 +52,24 @@ if __name__ == '__main__':
         print('Use "-c" argument to clear LEDs on exit')
 
     try:
-
-        while True:
-            print('Strobe 20hz.')
-            strobe(strip, 1000)
-            print('Color wipe animations.')
-            colorWipe(strip, Color(255, 0, 0))  # Red wipe
-            colorWipe(strip, Color(0, 255, 0))  # Green wipe
-            colorWipe(strip, Color(0, 0, 255))  # Blue wipe
-            colorWipe(strip, Color(255, 255, 255))
-            print('Theater chase animations.')
-            theaterChase(strip, Color(127, 127, 127))  # White theater chase
-            theaterChase(strip, Color(127, 0, 0))  # Red theater chase
-            theaterChase(strip, Color(0, 0, 127))  # Blue theater chase
-            print('Rainbow animations.')
-            rainbow(strip)
-            rainbowCycle(strip)
-            theaterChaseRainbow(strip)
+        filename = 'test.bin'
+        print('Reading PieShow file... ['+filename+']')
+        
+        pieShowBin = None
+        with open(filename, mode='rb') as filestream:
+            pieShowBin = filestream.read()
+            
+        if pieShowBin is None:
+            print('Failed to read file! Exiting...')
+            sys.exit(1)
+        
+        print('Rendering show...')
+        renderPieShowFile(strip, pieShowBin)
+            
+        while args.loop
+            print('Looping show...')
+            renderPieShowFile(strip, pieShowBin)
 
     except KeyboardInterrupt:
         if args.clear:
-            colorWipe(strip, Color(0, 0, 0), 10)
+            setAll(strip, Color(0, 0, 0))
