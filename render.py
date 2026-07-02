a6a53cdc057cc5d4fbf4fe5e1fa5d9f19dcfc71c
thelightdesigner <styplanekyle@gmail.com>
2026-07-02 00:14:07 -0400

    needs cleaning

--- render.py
+++ render.py
@@ -12,7 +12,7 @@ from rpi_ws281x import PixelStrip, Color
 from PIL import Image
 import argparse
 
-LED_COUNT = 262     
+LED_COUNT = 650     
 LED_PIN = 18          
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
@@ -35,10 +35,11 @@ def renderPieShowFile(strip, img, fps):
     frameCount = 0
 
     while (now <= durationSeconds):
-        for led in range(width):
-            frame = math.floor(now * fps)
-            (r,g,b)  = pixels[led,frame]
-            strip.setPixelColor(led, Color(r, g, b))
+        frame = math.floor(now * fps)
+
+#        for led in range(width):
+#            (r,g,b)  = pixels[led,frame]
+#            strip.setPixelColor(led, Color(r, g, b))
 
         strip.show()
 
@@ -58,10 +59,39 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
     parser.add_argument('-l', '--loop', action='store_true', help='loop the file on completion')
+    parser.add_argument('-w','--white', action='store_true', help='make the whole strip white')
+    parser.add_argument('-s','--snake', action='store_true', help='')
     args = parser.parse_args()
 
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
+    
+    if args.snake:
+        black = Color(0,0,0)
+        white = Color(255, 255, 255)
+        index = 0
+        while True:
+            if (index >= LED_COUNT-15):
+                index = 0
+            setAll(strip, black)
+          #  for i in range(index, index+15):
+            strip.setPixelColor(index, white)
+     #       strip.show()
+     #       time.sleep(0.05)
+     #       setAll(strip, black)
+            strip.show()
+     #       time.sleep(0.05)
+            index += 1
+
+    if args.white:
+        while True:
+            setAll(strip, Color(100, 100, 100))
+            strip.show()
+            time.sleep(2)
+            setAll(strip, Color(255, 255, 255))
+            strip.show()
+            time.sleep(2)
+        exit()
 
     try:
         filename = 'test.png'
@@ -74,7 +104,7 @@ if __name__ == '__main__':
         
         while args.loop:
             print('Looping show...')
-            renderPieShowFile(strip, pieShowImage, 25)
+            renderPieShowFile(strip, pieShowImage, 5)
 
     except KeyboardInterrupt:
         if args.clear:
