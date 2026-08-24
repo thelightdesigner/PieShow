c3290e66ad57b17f7bed1713f9c74f99180aaee7
kmthoms2_ncstate <kmthoms2@ncsu.edu>
2026-08-23 23:00:16 -0400

    rainbow

--- render.py
+++ render.py
@@ -67,6 +67,19 @@ class LEDTape:
 
     def set(self, ch, led, color):
         self.strip.setPixelColor(ch, led, color)
+    
+    def setAllRainbow(self, index, scale, brightness)
+        for i in range(self.C1_LEN):
+            (r,g,b) = hsv2rgb(((i + index) / scale) % 1, 1,brightness / 255)
+            color = mRGBW(r,g,b)
+            self.strip.setPixelColor(0, i, color)
+        for i in range(self.C2_LEN):
+            (r,g,b) = hsv2rgb(((i + index) / scale) % 1, 1,brightness / 255)
+            color = mRGBW(r,g,b)
+            self.strip.setPixelColor(1, i, color)
 
     def show(self):
         self.strip.show()
+        
+    def hsv2rgb(h,s,v):
+        return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
